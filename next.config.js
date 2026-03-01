8ea70b72afe4cf8b7fe21b27fd6d5f8f113a6d50
jpwatterson-eng <jpwatterson@gmail.com>
2026-03-01 15:39:18 +0000

    Deploy service worker

--- next.config.js
+++ next.config.js
@@ -3,14 +3,41 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
+  disable: process.env.NODE_ENV === 'development',
+  cacheOnFrontEndNav: true,
+  reloadOnOnline: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/firestore\.googleapis\.com\/.*/i,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'firebase-cache',
+        networkTimeoutSeconds: 10,
+        expiration: {
+          maxEntries: 200,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+        },
+      },
+    },
+    {
+      urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com\/.*/i,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'google-fonts',
+        expiration: {
+          maxEntries: 20,
+          maxAgeSeconds: 365 * 24 * 60 * 60,
+        },
+      },
+    },
+  ],
 });
 
 const nextConfig = {
   reactStrictMode: true,
   distDir: '.next',
   output: 'standalone',
-  turbopack: {}, // Add this to silence the warning
+  turbopack: {},
 };
 
 module.exports = withPWA(nextConfig);
